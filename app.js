a73f4514facc1fa0163f50da91f60653dd6cb6ce
EduFLtec <70642283+EduFLtec@users.noreply.github.com>
2022-02-17 17:12:43 -0500

    Make gameBoard module

--- app.js
+++ app.js
@@ -1,11 +1,36 @@
-// modules
+ // modules
+const gameBoard = (() => {
+        const boardCells = document.querySelectorAll(".cell");
+        const boardArray = Array.from(boardCells);
+        
+        const getBoard = () => boardArray;
+    
+        const clearBoard = function(){
+          boardArray.forEach(function(item){
+            item.textContent = "";
+          })
 
-    // const gameBoard
+        }
 
-    // const displayController
+        return {
+          getBoard,
+          clearBoard
+
+        }
+
+  })();
+
+// const displayController 
+  //add event listeners for clicks
+
+
+
+
+
 
 // factories
     // const player1
+
 
     // const player2
 
